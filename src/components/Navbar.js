9c1402af50a435c2a8017ae93426087a59c5c8a0
Jeremy <josz1@student.monash.edu>
2019-08-11 16:18:16 +1000

    recolouring

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,36 +1,37 @@
-import React from 'react'
-import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import React from "react";
+import { Link } from "gatsby";
+import github from "../img/github-icon.svg";
+import logo from "../img/900x900-Logo.png";
+import styled from "styled-components";
 
 const Navbar = class extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       active: false,
-      navBarActiveClass: '',
-    }
+      navBarActiveClass: ""
+    };
   }
 
   toggleHamburger = () => {
     // toggle the active boolean in the state
     this.setState(
       {
-        active: !this.state.active,
+        active: !this.state.active
       },
       // after state has been updated,
       () => {
         // set the class in state for the navbar accordingly
         this.state.active
           ? this.setState({
-              navBarActiveClass: 'is-active',
+              navBarActiveClass: "is-active"
             })
           : this.setState({
-              navBarActiveClass: '',
-            })
+              navBarActiveClass: ""
+            });
       }
-    )
-  }
+    );
+  };
 
   render() {
     return (
@@ -41,7 +42,6 @@ const Navbar = class extends React.Component {
       >
         <div className="container">
           <div className="navbar-brand">
-            
             {/* Hamburger menu */}
             <div
               className={`navbar-burger burger ${this.state.navBarActiveClass}`}
@@ -58,14 +58,14 @@ const Navbar = class extends React.Component {
             className={`navbar-menu ${this.state.navBarActiveClass}`}
           >
             <div className="navbar-start has-text-centered">
-              <Link to="/" className="navbar-item" title="Logo">
-                <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-              </Link>
               <Link className="navbar-item" to="/about">
                 About
               </Link>
               <Link className="navbar-item" to="/products">
                 Projects
+              </Link>
+              <Link to="/" className="navbar-item" title="Logo">
+                <img src={logo} alt="Kaldi" style={{ width: "88px" }} />
               </Link>
               <Link className="navbar-item" to="/blog">
                 News
@@ -89,8 +89,8 @@ const Navbar = class extends React.Component {
           </div>
         </div>
       </nav>
-    )
+    );
   }
-}
+};
 
-export default Navbar
+export default Navbar;
--- src/components/SiteWrapper.js
+++ src/components/SiteWrapper.js
@@ -2,20 +2,52 @@ import React from "react";
 import styled from "styled-components";
 
 const SiteWrapper = styled.div`
+  @import url("https://fonts.googleapis.com/css?family=Amatic+SC|Open+Sans&display=swap");
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    font-family: "Amatic SC", Arial, Helvetica, sans-serif;
+  }
+  body {
+    font-family: "Open Sans", Arial, Helvetica, sans-serif;
+  }
+
   @media screen and (min-width: 1023px) {
     display: flex;
+    background-color: #000;
+    color: #fff;
+    .navbar-menu {
+      background: #000;
+    }
     .navbar-start {
       position: fixed;
       height: 100vh;
       justify-content: center;
       margin-right: auto;
-      /* display: flex; */
       flex-direction: column;
       width: 100px;
       left: 3rem;
+      padding: 5rem auto;
+      font-family: "Amatic SC", "Lucida Sans", "Lucida Sans Regular",
+        "Lucida Grande", "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
+      font-size: 1.75rem;
+      font-weight: 600;
+      color: #fff;
     }
     .navbar-menu {
       min-width: 220px;
+    }
+    .navbar-item {
+      display: flex;
+      flex: auto;
+      color: inherit;
+      :hover {
+        color: red;
+      }
     }
   }
 `;
